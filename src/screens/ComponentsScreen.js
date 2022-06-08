3de5385fd93069ba751c96e090f500bd9a7299c0
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-06-08 15:11:55 -0300

    feat: first exercise - getting started

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
+import { View } from 'react-native-web';
 
 const ComponentsScreen = () => {
-  return <Text style={styles.text}>Hi there!</Text>;
+  const name = 'Camilla';
+  
+  return (
+    <View> 
+    <Text style={styles.text}>Getting started with React Native!</Text>
+    <Text style={styles.textSubtitle}>Here is {name}</Text>
+    </View>
+ )
 };
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30,
+    fontSize: 45
   },
+  textSubtitle: {
+    fontSize: 20
+  }
 });
 
 export default ComponentsScreen;
