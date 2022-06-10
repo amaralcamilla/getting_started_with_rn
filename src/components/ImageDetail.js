6edbef02b550fb075c21776a261557f0a2536876
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-06-10 15:24:22 -0300

    feat: add ImageSource and ImageScore

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
-const ImageDetail = (props) => {
-    return <Text>{props.title}</Text>;
+const ImageDetail = props => {
+    return  <View>
+        <Image source={props.imageSource} />
+      <Text>{props.title}</Text> 
+      <Text>Image score - {props.imageScore}</Text> 
+    </View>
+    
 };
 
 const styles = StyleSheet.create({});
