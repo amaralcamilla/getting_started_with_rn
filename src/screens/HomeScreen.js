69e4c9e6f95edbf252c6635b04c29d404403a419
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-06-13 15:35:24 -0300

    feat: initialize SquareScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -25,6 +25,10 @@ const HomeScreen = ({navigation}) => {
     onPress={() => navigation.navigate('Color')}
     title="Go To Color Demo"
     />
+    <Button
+    onPress={() => navigation.navigate('Square')}
+    title="Go To Square Demo"
+    />
   </View>)
 };
 
