1a5f60d322fc6f72bc67174891ddf4b8fd6832b5
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-06-15 11:13:22 -0300

    feat: initializes TextScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,41 +1,46 @@
-import React from 'react';
-import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = ({navigation}) => {
-  return (
-  <View>
-    <Text style={styles.text}>Hi there!</Text>
-    <Button 
-    onPress={() => navigation.navigate('Components')}
-    title="Go to Components Demo" 
-    />
-    <Button
-    onPress={() => navigation.navigate('List')}
-    title="Go to List Demo"
-    />
-    <Button
-    onPress={() => navigation.navigate('Image')}
-    title="Go To Images Demo"
-    />
-     <Button
-    onPress={() => navigation.navigate('Counter')}
-    title="Go To Counter Demo"
-    />
-    <Button
-    onPress={() => navigation.navigate('Color')}
-    title="Go To Color Demo"
-    />
-    <Button
-    onPress={() => navigation.navigate('Square')}
-    title="Go To Square Demo"
-    />
-  </View>)
+const HomeScreen = ({ navigation }) => {
+	return (
+		<View>
+			<Text style={styles.text}>Hi there!</Text>
+			<Button
+				onPress={() => navigation.navigate("Components")}
+				title="Go to Components Demo"
+			/>
+			<Button
+				onPress={() => navigation.navigate("List")}
+				title="Go to List Demo"
+			/>
+			<Button
+				onPress={() => navigation.navigate("Image")}
+				title="Go To Images Demo"
+			/>
+			<Button
+				onPress={() => navigation.navigate("Counter")}
+				title="Go To Counter Demo"
+			/>
+			<Button
+				onPress={() => navigation.navigate("Color")}
+				title="Go To Color Demo"
+			/>
+			<Button
+				onPress={() => navigation.navigate("Square")}
+				title="Go To Square Demo"
+			/>
+			<Button
+				onPress={() => navigation.navigate("Text")}
+				title="Go To Text Demo"
+			/>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 30,
-  },
+	text: {
+		fontSize: 30,
+	},
 });
 
 export default HomeScreen;
