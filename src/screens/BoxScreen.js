94b201a9c3ffa0afea4476606b23b1b94b4c3b18
Camilla Amaral <camilla.amaral@paripassu.com.br>
2022-06-17 11:57:02 -0300

    feat: exercise #5 - add layout on BoxScreen

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,12 +3,38 @@ import { View, Text, StyleSheet } from "react-native";
 
 const BoxScreen = () => {
 	return (
-		<View>
-			<Text>Box Screen</Text>
+		<View style={styles.parentStyle}>
+			<View style={styles.viewOneStyle} />
+			<View style={styles.viewTwoStyle} />
+			<View style={styles.viewThreeStyle} />
 		</View>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	parentStyle: {
+		borderWidth: 1,
+		borderColor: "black",
+		height: 200,
+		flexDirection: "row",
+		justifyContent: "space-between",
+	},
+	viewOneStyle: {
+		backgroundColor: "red",
+		height: 50,
+		width: 50,
+	},
+	viewTwoStyle: {
+		backgroundColor: "green",
+		height: 50,
+		width: 50,
+		top: 50,
+	},
+	viewThreeStyle: {
+		backgroundColor: "purple",
+		height: 50,
+		width: 50,
+	},
+});
 
 export default BoxScreen;
